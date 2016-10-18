5b2ccb7b6315f4d971fce123023dd4f8e1bc227a
realbot <realbot@w3soft.ru>
2016-10-18 13:09:48 +0300

    Replace Object.assign to lodash/assign

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import StateMachine from 'javascript-state-machine';
 import map from 'lodash/map';
+import assign from 'lodash/assign';
 import zipObject from 'lodash/zipObject';
 import fill from 'lodash/fill';
 
@@ -17,7 +18,7 @@ export const defaultState =
  */
 const reducerBuilder = (fsmConfig) =>
 {
-  const fsm = StateMachine.create(Object.assign({}, fsmConfig, { initial }));
+  const fsm = StateMachine.create(assign({}, fsmConfig, { initial }));
   const { events } = fsmConfig;
   const eventNames = map(events, 'name');
   const eventsCount = eventNames.length;
